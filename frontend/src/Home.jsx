4b26ead7e5acb30b8f2e1ab3137f9da88734ad17
Eduardo Tumimbo <eduardotumimbo.dev@gmail.com>
2025-01-28 11:09:33 +0800

    add Navbar component and enhance Home layout with responsive grid

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,5 +1,29 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./components/Navbar.jsx";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <Grid
+        templateColumns={{ base: "1fr", md: "1fr 2fr 1fr" }}
+        height="100vh"
+        p={4}
+      >
+        <GridItem bg="gray.100" p={4} display={{ base: "none", md: "block" }}>
+          Left Sidebar
+        </GridItem>
+
+        <GridItem bg="gray.200" p={4}>
+          Main Content
+        </GridItem>
+
+        <GridItem bg="gray.100" p={4} display={{ base: "none", md: "block" }}>
+          Right Sidebar
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { Flex, Text, Link } from "@chakra-ui/react";
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from "@/components/ui/menu";
+
+function Navbar() {
+  return (
+    <Flex
+      bg="teal.500"
+      color="white"
+      p={4}
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Text fontSize="xl" fontWeight="bold">
+        Reblog
+      </Text>
+
+      <Flex alignItems="center" gap={4}>
+        <Link href="#" colorPalette="accent" _focus={{ outline: "none" }}>
+          Home
+        </Link>
+
+        <Link href="#" _focus={{ outline: "none" }}>
+          Create Post
+        </Link>
+
+        <MenuRoot>
+          <MenuTrigger asChild>
+            <Link href="#" _focus={{ outline: "none" }}>
+              John Doe
+            </Link>
+          </MenuTrigger>
+          <MenuContent>
+            <MenuItem value="new-txt">Profile</MenuItem>
+            <MenuItem value="new-file">Logout</MenuItem>
+          </MenuContent>
+        </MenuRoot>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Navbar;
