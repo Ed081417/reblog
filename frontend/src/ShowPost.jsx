70811a7251ae2e00e41b0e6562c245076d6b1273
Eduardo Tumimbo <eduardotumimbo.dev@gmail.com>
2025-01-30 12:30:49 +0800

    Show Post UI

--- frontend/src/ShowPost.jsx
+++ frontend/src/ShowPost.jsx
@@ -1,11 +1,29 @@
+import { Card, Heading, Text, Center, Image } from "@chakra-ui/react";
 import Navbar from "./components/Navbar.jsx";
+import CarImg from "./assets/images/car1.jpg";
 
 function ShowPost() {
   return (
     <>
       <Navbar />
 
-      <h1>Show Post</h1>
+      <Center marginTop="10">
+        <Card.Root maxW="xl" width="100%">
+          <Card.Header>
+            <Heading size="2xl">Tokyo Drift</Heading>
+          </Card.Header>
+          <Card.Body color="fg.muted">
+            <Text marginBottom="4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+              obcaecati ex autem placeat, iusto laborum atque omnis sapiente
+              nostrum deserunt labore itaque minima maxime accusantium
+              aspernatur ipsa eius dicta consequuntur.
+            </Text>
+
+            <Image src={CarImg} alt="car" />
+          </Card.Body>
+        </Card.Root>
+      </Center>
     </>
   );
 }
